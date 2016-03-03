2fad1045fd1537187d802e243601ed0cd1790603
Y2Que <james.garza.a@gmail.com>
2016-03-02 21:13:49 -0500

    Proper error checking
    instead of not pulling the query results, say "access denied" if user is
    in the system

--- Pi_Database/pi.php
+++ Pi_Database/pi.php
@@ -1,6 +1,7 @@
 <html>
  <body>
 <?php
+/*
 	if ($_GET) {
 		$argument1 = $_GET['argument1'];
 		$argument1 = $_GET['argument1'];
@@ -8,6 +9,7 @@
 		$rfidValue = $argv[1];
 		$timeRequest = $argv[2];
 	}
+	*/
 	
 	$servername = "172.99.97.197:4120";
 	$username = "475692_request";
@@ -22,18 +24,18 @@
 	$selected = mssql_select_db($db, $dbhandle)
 	or die("Couldn't open database $db"); 	
 	
-	//$rfidValue = "18460";
-	//$timeRequest = "2016-01-01 00:00:00";
+	$rfidValue = "18460";
+	$roomRequest = "206"
+	$timeRequest = "2016-02-20 12:40:00";
 
 	
 	$strSQL = "SELECT U.Name, U.RFID, E.Event, E.Approved, E.Room, E.StartTime, E.EndTime
 			   FROM tbl_Users AS U
 			   RIGHT JOIN tbl_Events AS E
 			   ON U.RFID = E.RFID
-			   WHERE E.Approved = 1 
-			   AND U.RFID = " . $rfidValue . " 
-			   AND E.StartTime < '" . $timeRequest . "' 
-			   AND E.EndTime > '" . $timeRequest . "'";
+			   WHERE U.RFID = " . $rfidValue; // . "
+			   //AND E.StartTime < '" . $timeRequest . "' 
+			   //AND E.EndTime > '" . $timeRequest . "'";
 	$query = mssql_query($strSQL);
 
 	// count the returned rows
@@ -44,13 +46,13 @@
 	} else {
 		$data = mssql_fetch_array($query);
 		$access = $data["Approved"];
+		$room = $data["Room"];
+		$startTime = $data["StartTime"];
+		$endTime = $data["EndTime"];
 		
-		if ($access) {
+		if ($access && ($room == $roomRequest) && ($startTime > $timeRequest) && ($endTime > $timeRequest)) {
 			$name = $data["Name"];
 			$event = $data["Event"];
-			$room = $data["Room"];
-			$startTime = $data["StartTime"];
-			$endTime = $data["EndTime"];
 			
 			//$newtimestamp = strtotime("+20 days", $yourtimestamp);
 			
