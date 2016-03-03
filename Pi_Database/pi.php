7bc6f062b69eb555e6790ed67867f844a9af0ca7
Y2Que <james.garza.a@gmail.com>
2016-03-02 20:07:49 -0500

    modified query
    removed whitespace

--- Pi_Database/pi.php
+++ Pi_Database/pi.php
@@ -16,12 +16,7 @@
 
 	echo "Connected<br>";
 	
-	strSQL = "SELECT U.Name, U.RFID, E.Event, E.Approved, E.Room, E.StartTime, E.EndTime 
-			  FROM tbl_Users AS U
-			  RIGHT JOIN tbl_Events AS E
-			  ON U.RFID = E.RFID
-			  WHERE E.Approved = 1 AND U.RFID = 18460
-			  ORDER BY E.StartTime"; // WHERE E.Approved = 0
+	strSQL = "SELECT U.Name, U.RFID, E.Event, E.Approved, E.Room, E.StartTime, E.EndTime FROM tbl_Users AS U RIGHT JOIN tbl_Events AS E ON U.RFID = E.RFID WHERE E.Approved = 1 AND U.RFID = 18460 ORDER BY E.StartTime";
 	$query = mssql_query($strSQL);
 	
 	echo "Queried: ";
